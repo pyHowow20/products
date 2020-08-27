b7eb914bb593b0d3ea1f0ec6d2a85273940d5ff0
pyHoward <wang.howard@gmail.com>
2020-08-27 13:58:29 +0800

    revise products.py

--- products.py
+++ products.py
@@ -5,9 +5,8 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
-    p = [name, price]
-	products.append(p) #把小清單P放入大清單procucts
-#	products.append([name, price])
+	products.append([name, price]) #把小清單P放入大清單procucts
 print(products)
-print(products[1])
-print(products[0][1])
+
+for p in products:
+	print(p[0], '的價格是 ', p[1])
