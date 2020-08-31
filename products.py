68db375c71e161e0995efd97987f199cd79fcb1a
pyHoward <wang.howard@gmail.com>
2020-08-31 09:09:42 +0800

    Fixed a bug

--- products.py
+++ products.py
@@ -2,7 +2,7 @@
 products = []
 with open('products.csv', 'r', encoding='utf-8') as f:
 	for line in f:
-		if '商品, 價格' in line:
+		if '商品,價格' in line:
 			continue # 繼續
 		name, price = line.strip().split(',') # split完會變清單
 		products.append([name, price])
@@ -23,6 +23,5 @@ for p in products:
 
 # 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品,價格\n') # 欄位名稱
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n') #字串可以+合併
